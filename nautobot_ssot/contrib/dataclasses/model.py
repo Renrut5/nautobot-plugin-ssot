156d3abf346c302fb1c9ea80e99d8402eac78233
Andrew Turner <aj.turner@live.com>
2025-06-06 14:55:46 -0500

    update

--- nautobot_ssot/contrib/dataclasses/model.py
+++ nautobot_ssot/contrib/dataclasses/model.py
@@ -10,7 +10,7 @@ from nautobot_ssot.contrib.dataclasses.attributes import AttributeInterface, att
 
 @dataclass
 class NautobotModelInterface:
-    """Interface class for interacting with Nautobot ORM models."""
+    """Interface class for interacting with Nautobot ORM instances."""
 
     diffsync_class: NautobotModel
     cache: ORMCache = field(repr=False, default_factory=lambda : ORMCache())
@@ -19,10 +19,19 @@ class NautobotModelInterface:
 
     def __post_init__(self):
         self.type_hints = get_type_hints(self.diffsync_class, include_extras=True)
-        self.synced_attributes = self.diffsync_class.get_parameters()
         self.attribute_interfaces = {}
         for attribute in self.diffsync_class.get_parameters():
-            self.attribute_interfaces[attribute] = attribute_interface_factory()
+            self.attribute_interfaces[attribute] = attribute_interface_factory(
+                name=attribute,
+                type_hints=self.type_hints[attribute],
+                model_class=self.diffsync_class._model,  # pylint: disable=protected-access
+                cache=ORMCache,
+            )
+
+    @property
+    def synced_attributes(self):
+        """Return a list of synced attributes."""
+        return self.attribute_interfaces.keys()
 
     def get_parameters(self, db_obj: BaseModel) -> Dict:
         """Return a dictionary of all parameters and their values for passed Nautobot Database Models."""
