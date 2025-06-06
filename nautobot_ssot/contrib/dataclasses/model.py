4e9642f4706eb7d21b8686e0235ca5d56bae6734
Andrew Turner <aj.turner@live.com>
2025-06-06 14:45:29 -0500

    add files

---
+++ nautobot_ssot/contrib/dataclasses/model.py
@@ -0,0 +1,32 @@
+
+
+from dataclasses import dataclass, field
+from nautobot_ssot.contrib.model import NautobotModel
+from nautobot.core.models import BaseModel
+from typing_extensions import List, Any, Optional, Dict, get_type_hints
+from nautobot_ssot.contrib.dataclasses.cache import ORMCache
+from nautobot_ssot.contrib.dataclasses.attributes import AttributeInterface, attribute_interface_factory
+
+
+@dataclass
+class NautobotModelInterface:
+    """Interface class for interacting with Nautobot ORM models."""
+
+    diffsync_class: NautobotModel
+    cache: ORMCache = field(repr=False, default_factory=lambda : ORMCache())
+    attribute_interfaces: Dict[str, AttributeInterface] = field(repr=False, init=False)
+    type_hints: dict = field(init=False, repr=False)
+
+    def __post_init__(self):
+        self.type_hints = get_type_hints(self.diffsync_class, include_extras=True)
+        self.synced_attributes = self.diffsync_class.get_parameters()
+        self.attribute_interfaces = {}
+        for attribute in self.diffsync_class.get_parameters():
+            self.attribute_interfaces[attribute] = attribute_interface_factory()
+
+    def get_parameters(self, db_obj: BaseModel) -> Dict:
+        """Return a dictionary of all parameters and their values for passed Nautobot Database Models."""
+        parameters = {}
+        for attribute_name, interface in self.attribute_interfaces.items():
+            parameters[attribute_name] = interface.load(db_obj)
+        return parameters
